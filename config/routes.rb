137d5f91c9f28bec21ea546b18b2d1e2f35050d4
Vu Duy Tu <duytu131995@gmail.com>
2020-06-12 15:12:45 +0700

    Chapter3: Mostly static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
-    resources :test
+    root "static_pages#home"
+
+    get "home", to: "static_pages#home", as: "home"
+    get "help", to: "static_pages#help", as: "help"
   end
 end
