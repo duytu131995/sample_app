07a1cfee77c44371bffcde42b7e41d96b22074cf
Vu Duy Tu <duytu131995@gmail.com>
2020-06-17 11:45:25 +0700

    Chapter 7: Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,34 @@
 class UsersController < ApplicationController
-  def new; end
+  before_action :load_user, only: :show
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      flash[:danger] = t ".danger"
+      render :new
+    end
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t ".notice_id"
+    redirect_to root_path
+  end
+
+  def user_params
+    params.require(:user).permit User::USER_PARAMS
+  end
 end
